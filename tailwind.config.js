faafde8aba8a1e2fc8c9ba5c4a71a707ee86d2c0
mauricio riquelme <m.riquelme223@gmail.com>
2021-04-07 21:28:26 +1000

    adding style

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,33 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          LIGHTER: 'var(--colour-primary-lighter)',
+          LIGHT: 'var(--colour-primary-light',
+          DEFAULT: 'var(--colour-primary-default)',
+        },
+        secondary: {
+          DEFAULT: 'var(--colour-secondary-default)',
+        },
+        success: {
+          DEFAULT: 'var(--colour-success-default)',
+        },
+        warning: {
+          DEFAULT: 'var(--colour-warning-default)',
+        },
+        danger: {
+          DEFAULT: 'var(--colour-danger-default)',
+        },
+        neutral: {
+          DEFAULT: 'var(--colour-neutral-default)',
+        },
+        white: {
+          DEFAULT: 'var(--colour-white-default)',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
