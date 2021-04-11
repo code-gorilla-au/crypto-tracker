491ceb5b8f636699d37a1cbb31deccd605e173d5
mauricio riquelme <m.riquelme223@gmail.com>
2021-04-11 10:35:58 +1000

    formatting

--- src/components/CurrencyTable.vue
+++ src/components/CurrencyTable.vue
@@ -3,8 +3,23 @@
     <table class="min-w-full bg-white">
       <thead class="justify-between">
         <tr class="bg-gray-800 text-white">
-          <th v-for="header in headers" :key="header" :class="headerStyle">
-            <span>{{ header }}</span>
+          <th :key="header" :class="headerStyle">
+            <span>#</span>
+          </th>
+          <th :key="header" :class="headerStyle">
+            <span>Name</span>
+          </th>
+          <th :key="header" :class="headerStyle">
+            <span>Current Price</span>
+          </th>
+          <th :key="header" :class="headerStyle">
+            <span>Market Cap</span>
+          </th>
+          <th :key="header" :class="headerStyle">
+            <span>Total Volume</span>
+          </th>
+          <th :key="header" :class="headerStyle">
+            <span>Change %</span>
           </th>
         </tr>
       </thead>
@@ -17,17 +32,17 @@
             <img class="currency-image" :src="currency.image" :alt="currency.name" />
             <span>{{ currency.name }}</span>
           </td>
-          <td class="text-left py-3 px-4">
+          <td class="text-right py-3 px-4">
             <span>{{ currency.current_price }}</span>
           </td>
 
-          <td class="text-left py-3 px-4">
+          <td class="text-right py-3 px-4">
             <span>{{ currency.market_cap }}</span>
           </td>
-          <td class="text-left py-3 px-4">
+          <td class="text-right py-3 px-4">
             <span>{{ currency.total_volume }}</span>
           </td>
-          <td class="text-left py-3 px-4">
+          <td class="text-right py-3 px-4">
             <span>{{ currency.price_change_percentage_24h }}</span>
           </td>
         </tr>
@@ -50,7 +65,7 @@ export default defineComponent({
   },
   setup(props) {
     const headers = ['#', 'name', 'current price', 'market cap', 'total volume', 'change %'];
-    const headerStyle = computed(() => 'w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm');
+    const headerStyle = computed(() => 'w-1/3 text-left py-3 px-4 font-semibold text-sm');
     const rowStyle = computed(() => 'flex justify-start items-center w-1/7 py-3 px-4');
     return {
       list: props.currencies,
