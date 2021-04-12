1e4d5ce5c41868a1e32fd043cb10dfcc4785a0e0
mauricio riquelme <m.riquelme223@gmail.com>
2021-04-12 21:19:51 +1000

    adding base table

---
+++ src/components/BaseTable.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="flex w-full md:w-auto">
+    <table class="min-w-full bg-white">
+      <TableHead>
+        <TableHeader v-for="header in headers" :key="header[index]">
+          <span>{{ header }}</span>
+        </TableHeader>
+      </TableHead>
+
+      <tbody class="text-gray-700">
+        <tr class="flex  flex-col md:table-row" v-for="row in data" :key="row">
+          <slot name="props" :row="row"></slot>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import TableHead from '@/components/TableHead.vue';
+import TableHeader from '@/components/TableHeader.vue';
+
+export default defineComponent({
+  name: 'BaseTable',
+  components: {
+    TableHead,
+    TableHeader,
+  },
+  props: {
+    headers: {
+      type: Array as PropType<string[]>,
+      required: true,
+    },
+    data: {
+      type: Array,
+      required: false,
+      default: () => [],
+    },
+  },
+});
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import BaseIcon from '@/components/BaseIcon.vue';
+import BaseTable from '@/components/BaseTable.vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -9,4 +10,5 @@ createApp(App)
   .use(store)
   .use(router)
   .component('base-icon', BaseIcon)
+  .component('base-table', BaseTable)
   .mount('#app');
